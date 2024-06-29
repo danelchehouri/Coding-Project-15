09af4fa1d885364d78d3b9ad0a86222ff14d8acd
Dan El Chehouri <danelchehouri@gmail.com>
2024-06-28 23:46:38 -0400

    Create Personal info.js

---
+++ Personal info.js
@@ -0,0 +1,15 @@
+// src/components/PersonalInfo.js
+import React from 'react';
+import './PersonalInfo.css';
+
+const PersonalInfo = () => {
+  return (
+    <div className="personal-info">
+      <h1>Daniella (Dan) El Chehouri</h1>
+      <p>Tampa Bay, FL | danelchehouri@gmail.com | (813) 468-3971 | <a href="https://www.linkedin.com/in/danelchehouri" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p>I’m a Business Analytics and Information Systems student with a passion for social media and the beauty industry. I combine my data skills with a creative mindset to spot trends, amp up engagement, and develop smart strategies. Excited to bring fresh ideas and create impactful content for innovative brands!</p>
+    </div>
+  );
+}
+
+export default PersonalInfo;
