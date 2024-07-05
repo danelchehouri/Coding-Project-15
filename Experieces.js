5e9b35f2f070986e201ba0c9de8be56f7fa59124
Dan El Chehouri <danelchehouri@gmail.com>
2024-07-05 19:22:39 -0400

    Create Experieces.js

---
+++ Experieces.js
@@ -0,0 +1,41 @@
+// src/components/Experiences.js
+import React from 'react';
+import Experience from './Experience';
+import './Experiences.css';
+
+const experiencesData = [
+  {
+    title: 'Marketing Intern',
+    company: 'Mind4Youth',
+    duration: 'Nov 2023 – Dec 2023',
+    location: 'Remote',
+    description: [
+      'Assisted in the creation and execution of marketing campaigns across different platforms.',
+      'Created and published engaging social media content that aligns with the brand and campaign messaging.',
+      'Monitored analytics to measure performance and social engagement.',
+      'Conducted market research and analysis to identify target audiences and opportunities for growth.',
+    ]
+  },
+  {
+    title: 'Marketing & Business Management Intern',
+    company: 'Global Food Services Inc.',
+    duration: 'May 2022 – August 2022',
+    location: 'Tampa, FL',
+    description: [
+      'Actively contributed to business growth by engaging in brainstorming sessions and aiding in the development of innovative marketing ideas for collateral digital content.',
+      'Facilitated business opportunities by scheduling and coordinating calls, meetings, and conference calls.',
+      'Enhanced the company’s digital presence by executing social media marketing campaigns.',
+    ]
+  },
+  {
+    title: 'Finance Intern',
+    company: 'Suncoast Credit Union',
+    duration: 'June 2020 – August 2020',
+    location: 'Tampa, FL',
+    description: [
+      'Collaborated with a team of interns to develop and deliver engaging financial literacy presentations.',
+      'Led the creation of a comprehensive business plan for a product from inception to presentation addressing market needs.',
+      'Engaged in different training sessions and discussions on business cases with stakeholders and departments.',
+    ]
+  }
+];
