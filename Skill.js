4b0c4928fd5d168a3f2745482f9d777da30bc9e8
Dan El Chehouri <danelchehouri@gmail.com>
2024-07-05 19:20:46 -0400

    Create Skill.js

---
+++ Skill.js
@@ -0,0 +1,15 @@
+// src/components/Skills.js
+import React from 'react';
+import Skill from './Skill';
+import './Skills.css';
+
+const skillsData = [
+  { name: 'Microsoft Office', level: 'Advanced' },
+  { name: 'Canva', level: 'Advanced' },
+  { name: 'CapCut', level: 'Intermediate' },
+  { name: 'Splice', level: 'Intermediate' },
+  { name: 'Hootsuite', level: 'Intermediate' },
+  { name: 'HTML', level: 'Intermediate' },
+  { name: 'Front-end Web Development', level: 'Intermediate' },
+  { name: 'JavaScript', level: 'Intermediate' },
+];
