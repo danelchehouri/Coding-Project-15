a2b4eaa673f01c819c515049a980677e37803e57
Dan El Chehouri <danelchehouri@gmail.com>
2024-07-06 23:59:33 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+//U20452568
+import React from 'react';
+import PersonalInfo from './PersonalInfo';
+import Skills from './Skills';
+import Experience from './Experience';
+import Education from './Education';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonalInfo />
+      <Skills />
+      <Experience />
+      <Education />
+    </div>
+  );
+}
